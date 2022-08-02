6aceab88381e2c6833b02f3b6bf19a09093ec182
shalnsingh@outlook.com <shalnsingh@outlook.com>
2022-08-02 20:57:57 +0530

    added Sidebar Component

---
+++ web_crawler/front-end/src/components/Sidebar.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import styles from "../styles/sidebar.module.css";
+
+const Sidebar = () => {
+    const sideMenu=[{
+        "summary":['Overview'],
+        "icon":["xyz"]
+    },
+    {
+        
+            "ORGANIZATION":['People','Teams/Skills'],
+            "icon":["xyz"] 
+    },
+    {
+        "LEARNING":['Learning Paths', "Assessments", "Settings"],
+        "Icon":['abc','xyz','pqr']
+    }
+        
+    ]
+  return (
+    <>
+    <div className={styles.main}>
+        <div className={styles.navTop}>
+            <div className={styles.navTop_inner}><h4>MEETBEANS</h4> 
+          
+            </div>
+             <p className={styles.sub_text}>powered by yogya.ai</p>
+        </div>
+        <div>
+                <div>
+                      <div className={styles.sub_section}>
+                    
+                <div className={styles.sub_section_title}>
+                    SUMMARIES
+                </div>
+                <div className={styles.sub_section_item}>
+                    Overview
+                </div>
+            </div>
+                </div>
+            
+          
+            <div className={styles.sub_section}>
+                <div className={styles.sub_section_title}>
+                    SUMMARIES
+                </div>
+                <div className={styles.sub_section_item}>
+                    Overview
+                </div>
+                <div className={styles.sub_section_item}>
+                    Overview
+                </div>
+            </div>
+            <div className={styles.sub_section}>
+                <div className={styles.sub_section_title}>
+                    SUMMARIES
+                </div>
+                <div className={styles.sub_section_item}>
+                    Overview
+                </div>
+                <div className={styles.sub_section_item}>
+                    Overview
+                </div>
+                <div className={styles.sub_section_item}>
+                    Overview
+                </div>
+            </div>
+        </div>
+
+    </div>
+    </>
+  )
+}
+
+export default Sidebar
