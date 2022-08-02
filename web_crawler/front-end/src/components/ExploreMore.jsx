7bdd06d5c37dad4e52bf5ecce799a47a7ea696c6
shalnsingh@outlook.com <shalnsingh@outlook.com>
2022-08-03 04:25:57 +0530

    updated ExploreMore Component

--- web_crawler/front-end/src/components/ExploreMore.jsx
+++ web_crawler/front-end/src/components/ExploreMore.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 
 import styles from "../styles/explore_more.module.css";
 
+import icon1 from '../images/background_img.jpg';
+
+
 const ExploreMore = () => {
   return (
     <>
-    <div className={styles.outer}>
-        <div className={styles.data}>
+    <div className={styles.outer} >
+        <div className={styles.data} style={{ "backgroundImage": `url(${icon1})`,"backgroundPosition": "center", "backgroundSize": "cover",
+    "backgroundRepeat": "no-repeat"}}>
+          <div className={styles.grad}>
+          <div className={styles.content}>
+            Act Before it's too late!
+          </div>
+          </div>
+        
+         
 
         </div>
 
