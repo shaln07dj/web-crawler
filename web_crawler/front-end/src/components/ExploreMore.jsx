53c4af24a4877f508d42abfc3eef3b18243083f7
shalnsingh@outlook.com <shalnsingh@outlook.com>
2022-08-02 20:32:37 +0530

    adding the ExploreMore Component

---
+++ web_crawler/front-end/src/components/ExploreMore.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import styles from "../styles/explore_more.module.css";
+
+const ExploreMore = () => {
+  return (
+    <>
+    <div className={styles.outer}>
+        <div className={styles.data}>
+
+        </div>
+
+    </div>
+    </>
+  )
+}
+
+export default ExploreMore
